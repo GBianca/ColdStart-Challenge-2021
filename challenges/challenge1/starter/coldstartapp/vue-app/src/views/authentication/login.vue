937ae6db91282c9d817c9e506e1cadc40d542b20
Bianca Ghita <bianca.ghita@ucb.com>
2021-02-24 00:27:27 +0100

    Adding login

---
+++ challenges/challenge1/starter/coldstartapp/vue-app/src/views/authentication/login.vue
@@ -0,0 +1,22 @@
+<script>
+import AuthLogin from '@/components/auth-login.vue';
+
+export default {
+  name: 'Login',
+  components: {
+    AuthLogin,
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<template>
+<auth-login provider="Facebook">
+<auth-login provider="Google">
+<auth-login provider="GitHub">
+</template>
