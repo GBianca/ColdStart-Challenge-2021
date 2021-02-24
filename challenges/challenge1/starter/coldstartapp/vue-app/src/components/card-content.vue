466a8c15429162c6a743794b248142129e4dcfee
Bianca Ghita <bianca.ghita@ucb.com>
2021-02-24 02:11:46 +0100

    Adding call to orders

--- challenges/challenge1/starter/coldstartapp/vue-app/src/components/card-content.vue
+++ challenges/challenge1/starter/coldstartapp/vue-app/src/components/card-content.vue
@@ -1,4 +1,7 @@
 <script>
+import axios from 'axios';
+import API from '../store/config';
+
 export default {
   name: 'CardContent',
   props: {
@@ -20,6 +23,22 @@ export default {
     },
   },
   methods: {
+    async placeOrder() {
+      axios.post(`${API}/orders`, {
+        Id: '0B476647-586A-EB11-9889-000D3AB17657',
+        User: 'Pip Doe',
+        Date: '2021-02-08T21:54:56.260Z',
+        IcecreamId: 1,
+        Status: 'New',
+        DriverId: null,
+        FullAddress: '1 Microsoft Way, Redmond, WA 98052, USA',
+        LastPosition: null,
+      }).then((response) => {
+        alert(response);
+      }, (error) => {
+        alert(error);
+      });
+    },
   },
 };
 </script>
@@ -36,5 +55,8 @@ export default {
       </div>
       <p class="description">{{ description }}</p>
     </div>
+    <div class="card-action">
+      <button v-on:click="placeOrder">Preorder</button>
+  </div>
   </div>
 </template>
