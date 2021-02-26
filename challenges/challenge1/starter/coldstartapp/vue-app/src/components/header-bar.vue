a77cba7db93798a3b61ae319ff244a489d47ca22
Bianca Ghita <bianca.ghita@ucb.com>
2021-02-26 08:26:23 +0100

    Checking user info for null instead of undefined

--- challenges/challenge1/starter/coldstartapp/vue-app/src/components/header-bar.vue
+++ challenges/challenge1/starter/coldstartapp/vue-app/src/components/header-bar.vue
@@ -31,8 +31,8 @@ export default {
         </div>
       </div>
     <div class="navbar-end">
-      <auth-logout v-if="userInfo !== undefined"/>
-      <auth-login v-if="userInfo === undefined"  provider="github"/>
+      <auth-logout v-if="userInfo !== null"/>
+      <auth-login v-if="userInfo === null"  provider="github"/>
       </div>
     </nav>
   </header>
